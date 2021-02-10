62f385d94c4db524c6e7353f81f1439b9013afc4
Al <theonlypurplecat@gmail.com>
2021-02-11 12:26:06 +1300

    Basic Menu Base

---
+++ TenSecondBombGame/Assets/Scripts/MenuControls.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControls : MonoBehaviour
+{
+
+    // in menu pages/changes:
+    [SerializeField]
+    private GameObject Menu = null;
+    [SerializeField]
+    private GameObject QuitPrompt = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Start the Game things:
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+
+    //Quit things:
+
+    //when selecting the quit button bring up the Quit Yes/no option
+    public void QuitPromptOn()
+    {
+        QuitPrompt.SetActive(true);
+        Menu.SetActive(false);
+    }
+
+    //selecting No and closing the quit prompt
+    public void QuitPromptOff()
+    {
+        QuitPrompt.SetActive(false);
+        Menu.SetActive(true);
+    }
+
+    //selecting yes and quitting the game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
