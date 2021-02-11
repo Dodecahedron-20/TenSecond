39472ccddb2ea722aa1a18a47b45423c0bf73d31
Jet Yong <jettlag2000@gmail.com>
2021-02-11 15:24:44 +1300

    WireCursor

---
+++ TenSecondBombGame/Assets/Scripts/CursorController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    public Texture2D wireCursor;
+    public Texture2D defaultCursor;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    public static CursorController Instance { get; private set; }
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        if(Physics.Raycast(ray, out hitInfo, Mathf.Infinity))
+        {
+            if(hitInfo.collider.GetComponent<Wires>() !=null)
+            {
+                SetWireCursor();
+            }
+            else
+            {
+                SetDefaultCursor();
+            }
+        }
+    }
+
+    public void SetWireCursor()
+    {
+        Cursor.SetCursor(wireCursor, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void SetDefaultCursor()
+    {
+        Cursor.SetCursor(defaultCursor, Vector2.zero, CursorMode.Auto);
+    }
+}
--- TenSecondBombGame/Assets/Scripts/Rotate.cs
+++ TenSecondBombGame/Assets/Scripts/Rotate.cs
@@ -37,13 +37,4 @@ public class Rotate : MonoBehaviour
     {
         dragging = true;
     }
-    
-    //void OnMouseDrag()
-    //{
-    //    float rotX = Input.GetAxis("Mouse X") * speed * Mathf.Deg2Rad;
-    //    float rotY = Input.GetAxis("Mouse Y") * speed * Mathf.Deg2Rad;
-
-    //    transform.Rotate(Vector3.up, -rotX);
-    //    transform.Rotate(Vector3.right, -rotY);
-    //}
 }
