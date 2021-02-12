3de72ccdb61eca9382b7457a3c42c438fffe3355
Jet Yong <jettlag2000@gmail.com>
2021-02-12 15:23:15 +1300

    morewires

--- TenSecondBombGame/Assets/Scripts/Wires.cs
+++ TenSecondBombGame/Assets/Scripts/Wires.cs
@@ -4,9 +4,39 @@ using UnityEngine;
 
 public class Wires : MonoBehaviour
 {
+    public GameObject redWire;
+    public GameObject redWireCut;
+    public GameObject blueWire;
+    public GameObject blueWireCut;
+    public GameObject greenWire;
+    public GameObject greenWireCut;
+    public GameObject yellowWire;
+    public GameObject yellowWireCut;
+
     //public Texture2D cursorTexture;
     //public CursorMode cursorMode = CursorMode.Auto;
     //public Vector2 hotSpot = Vector2.zero;
+
+    void Start()
+    {
+        redWire.SetActive(true);
+        blueWire.SetActive(true);
+        greenWire.SetActive(true);
+        yellowWire.SetActive(true);
+
+        redWireCut.SetActive(false);
+        blueWireCut.SetActive(false);
+        greenWireCut.SetActive(false);
+        yellowWireCut.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+
+        }
+    }
 
     private void OnMouseEnter()
     {
