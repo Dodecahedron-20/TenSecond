5fc6ef5df72a2627e81cda698ec45ae0c86d35c2
Jet Yong <jettlag2000@gmail.com>
2021-02-15 15:24:46 +1300

    abitmorechange

--- TenSecondBombGame/Assets/Scripts/Wires.cs
+++ TenSecondBombGame/Assets/Scripts/Wires.cs
@@ -4,58 +4,66 @@ using UnityEngine;
 
 public class Wires : MonoBehaviour
 {
-    public GameObject redWire;
-    public GameObject redWireCut;
-    public GameObject blueWire;
-    public GameObject blueWireCut;
-    public GameObject greenWire;
-    public GameObject greenWireCut;
-    public GameObject yellowWire;
-    public GameObject yellowWireCut;
+    RaycastHit hit;
+    public GameObject[] wires;
+    public GameObject[] cutWires;
 
-    //public Texture2D cursorTexture;
-    //public CursorMode cursorMode = CursorMode.Auto;
-    //public Vector2 hotSpot = Vector2.zero;
+    int RedWire;
+    int BlueWire;
+    int GreenWire;
+    int YellowWire;
 
     void Start()
     {
-        redWire.SetActive(true);
-        blueWire.SetActive(true);
-        greenWire.SetActive(true);
-        yellowWire.SetActive(true);
-
-        redWireCut.SetActive(false);
-        blueWireCut.SetActive(false);
-        greenWireCut.SetActive(false);
-        yellowWireCut.SetActive(false);
+        for(int i =0; i < cutWires.Length; i++)
+        {
+            cutWires[i].SetActive(false);
+        }
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        if(Input.GetMouseButtonDown(0))
         {
+            if(Physics.Raycast(Camera.main.ScreenPointToRay (Input.mousePosition), out hit, Mathf.Infinity, RedWire))
+            {
+                Debug.Log("Red Wire Pressed");
+                wires[1].SetActive(false);
+                cutWires[1].SetActive(true);
+            }
 
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, BlueWire))
+            {
+                Debug.Log("Blue Wire Pressed");
+                wires[2].SetActive(false);
+                cutWires[2].SetActive(true);
+            }
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, GreenWire))
+            {
+                Debug.Log("Green Wire Pressed");
+                wires[3].SetActive(false);
+                cutWires[3].SetActive(true);
+            }
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, YellowWire))
+            {
+                Debug.Log("Yellow Wire Pressed");
+                wires[4].SetActive(false);
+                cutWires[4].SetActive(true);
+            }
         }
     }
 
-    private void OnMouseDown()
-    {
-        if(gameObject.tag == "RedWire")
-        {
-            Instantiate(redWireCut, transform.position, transform.rotation);
-            Destroy(gameObject);
-        }
-    }
+    //private void OnMouseEnter()
+    //{
+    //    //Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    //    CursorController.Instance.SetWireCursor();
+    //}
 
-    private void OnMouseEnter()
-    {
-        //Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
-        CursorController.Instance.SetWireCursor();
-    }
-
-    private void OnMouseExit()
-    {
-        //Cursor.SetCursor(null, Vector2.zero, cursorMode);
-        CursorController.Instance.SetDefaultCursor();
-    }
+    //private void OnMouseExit()
+    //{
+    //    //Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    //    CursorController.Instance.SetDefaultCursor();
+    //}
 }
